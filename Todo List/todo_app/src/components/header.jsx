89f3c9450f03df9a6bac42787939bbf0dc4d0d43
sahithi-bommineni <bommineni.s@northeastern.edu>
2025-07-21 16:28:18 -0500

    creating components

---
+++ Todo List/todo_app/src/components/header.jsx
@@ -0,0 +1,14 @@
+function Header(){
+    return(
+        <div>
+             <header class="header">
+                <h1>My ToDo App</h1>
+            </header>
+            <div class="welcome">
+                <p>Welcome to my todo app</p>
+            </div>
+        </div>
+           
+    );
+}
+export default Header;
---
+++ Todo List/todo_app/src/components/todolists.jsx
@@ -0,0 +1,12 @@
+function Todolists(){
+    return(
+        <div class="todolists">
+            <ul>
+                <li>
+                    <input type="checkbox"/>
+                </li>
+            </ul>
+        </div>
+    );
+}
+export default Todolists;
